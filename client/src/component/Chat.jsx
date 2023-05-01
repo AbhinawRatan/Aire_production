b6c78460e206295284737e007c5366d083b3b6de
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2023-05-02 02:37:34 +0530

    first

---
+++ client/src/component/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div className='h-screen w-screen bg-white'>
+        <h1 className='font-bold text-5xl'>Hello</h1>
+      <div className='w-[25%] bg-[#0c0c15]'></div>
+    </div>
+  )
+}
+
+export default Chat
